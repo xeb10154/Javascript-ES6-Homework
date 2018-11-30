047360783f0bf7190cd9db45ca093c9ffd14732a
Raymond Yau <raymondccyau@gmail.com>
2018-11-30 14:43:29 +0000

    classes converted except PubSub

---
+++ client/src/models/instrument_families.js
@@ -0,0 +1,25 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+class InstrumentFamilies{
+  constructor(data){
+    this.data = data;
+  }
+
+  bindEvents(){
+    PubSub.publish('InstrumentFamilies:data-ready', this.data);
+
+    PubSub.subscribe('SelectView:change', (evt) => {
+      const selectedIndex = evt.detail;
+      this.publishFamilyDetail(selectedIndex);
+    });
+  };
+
+  publishFamilyDetail(selectedIndex) {
+    const selectedFamily = this.data[selectedIndex];
+    PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+  };
+
+
+};
+
+module.exports = InstrumentFamilies;
---
+++ client/src/views/instrument_family_view.js
@@ -0,0 +1,52 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+class InstrumentFamilyView {
+  constructor(container){
+    this.container = container;
+  }
+
+  bindEvents(){
+    PubSub.subscribe('InstrumentFamilies:selected-family-ready', (evt) => {
+      const instrumentFamily = evt.detail;
+      this.render(instrumentFamily);
+    });
+  };
+
+  render (family) {
+    this.container.innerHTML = '';
+
+    const familyName = this.createElement('h2', family.name);
+    this.container.appendChild(familyName);
+
+    const familyDescription = this.createElement('p', family.description);
+    this.container.appendChild(familyDescription);
+
+    const instrumentListTitle = this.createElement('h3', 'Instruments include:');
+    this.container.appendChild(instrumentListTitle);
+
+    const instrumentList = this.createInstrumentList(family.instruments);
+    this.container.appendChild(instrumentList);
+  };
+
+  createElement (elementType, text) {
+    const element = document.createElement(elementType);
+    element.textContent = text;
+    return element;
+  };
+
+  createInstrumentList (instruments) {
+    const list = document.createElement('ul');
+
+    instruments.forEach((instrument) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = instrument;
+      list.appendChild(listItem);
+    });
+
+    return list;
+  };
+
+};
+
+
+module.exports = InstrumentFamilyView;
